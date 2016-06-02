ba586d2aa49b5024e5557ee1d17309b3353fe795
Pepper Lebeck-Jobe <pepper@gradle.com>
2016-06-02 18:18:05 +0100

    Use the latest release snapshot.
    - This also adds the missing checkstyle.xml file needed by the
      checkstyle plugin for the example to actually pass.

--- src/test/groovy/CheckstyleIntegrationSpec.groovy
+++ src/test/groovy/CheckstyleIntegrationSpec.groovy
@@ -22,6 +22,7 @@ dependencies {
 '''
         javaFile()
         testFile()
+        checkFile()
 
         when:
         def result = runTasksSuccessfully('clean', 'check')
@@ -30,7 +31,6 @@ dependencies {
         result.wasExecuted(':checkstyleMain')
         result.wasExecuted(':checkstyleTest')
 
-        new File(projectDir, 'build/checkstyle/checkstyle.xml').exists()
         new File(projectDir, 'build/reports/checkstyle/main.xml').exists()
         new File(projectDir, 'build/reports/checkstyle/test.xml').exists()
     }
@@ -46,6 +46,21 @@ public class Hello {
         System.out.println("Hello");
     }
 }
+'''
+    }
+
+    def void checkFile() {
+        File checkFile = new File(projectDir, "config/checkstyle/checkstyle.xml")
+        Files.createParentDirs(checkFile)
+        checkFile.text = '''
+<!DOCTYPE module PUBLIC
+    "-//Puppy Crawl//DTD Check Configuration 1.2//EN"
+    "http://www.puppycrawl.com/dtds/configuration_1_2.dtd">
+<module name="Checker">
+    <module name="TreeWalker">
+        <module name="ConstantName"/>
+    </module>
+</module>
 '''
     }
 
